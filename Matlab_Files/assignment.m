959357b7ce245e15244e3bf02b4d2541c55ac1c3
VermillionS <38578815+VermillionS@users.noreply.github.com>
2019-05-14 21:45:13 +1000

    Update assignment.m

--- Matlab_Files/assignment.m
+++ Matlab_Files/assignment.m
@@ -1,8 +1,58 @@
 clc; clear all; close all;
 warning('off','all');
-% Student: Tran Quang Huy - n10069275 
+% Student: Tran Quang Huy - n10069275
+% Student: Nathan Armishaw - n9157191
 
-%% PCA 
+%% Section A: Support Vector Machines 
+
+
+%% Section A: Bayes Classifiers
+%Code is relavent to (A) and (B)
+%Set the Training and Test data as well as other constants related to them
+Xtr1 = [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1];
+Xtr2 = [0,0,0,0,1,1,1,1,0,0,0,1,1,1,1,1];
+Ytr = [0,1,1,1,0,1,1,1,0,0,0,0,0,0,1,1];
+Xtest = [0 1; 1 0; 1 1];
+YtrLength = length(Ytr);
+Ytr0Length = length(Ytr(Ytr==0));
+Ytr1Length = length(Ytr(Ytr==1));
+
+%Find the probabilities needed to create Joint/naïve Bayes classifier
+%Find out the percentage occurrence of each possible class(Ytr) value
+%to do this divide number of occurrences by the length of the total array
+%considered. Repeat this process to calculate all probabilities needed for 
+%classification
+P_0 = length(Ytr(Ytr==0)) / YtrLength;
+P_1 = length(Ytr(Ytr==1)) / YtrLength;
+
+%Find probabilities for Joint Bayes classifier, these are the percentage
+%occurrence rates of an (X1,X2) combination for a specific y value
+%P(x1,x2|y)
+jbc_P_000 = length(Xtr1(Xtr1==0 & Xtr2==0 & Ytr == 0))/Ytr0Length;
+jbc_P_010 = length(Xtr1(Xtr1==0 & Xtr2==1 & Ytr == 0))/Ytr0Length;
+jbc_P_100 = length(Xtr1(Xtr1==1 & Xtr2==0 & Ytr == 0))/Ytr0Length;
+jbc_P_110 = length(Xtr1(Xtr1==1 & Xtr2==1 & Ytr == 0))/Ytr0Length;
+jbc_P_001 = length(Xtr1(Xtr1==0 & Xtr2==0 & Ytr == 1))/Ytr1Length;
+jbc_P_011 = length(Xtr1(Xtr1==0 & Xtr2==1 & Ytr == 1))/Ytr1Length;
+jbc_P_101 = length(Xtr1(Xtr1==1 & Xtr2==0 & Ytr == 1))/Ytr1Length;
+jbc_P_111 = length(Xtr1(Xtr1==1 & Xtr2==1 & Ytr == 1))/Ytr1Length;
+
+%Find probabilities for naïve Bayes classifier, these are the percentage
+%occurrence rates of P(x1|y) and P(x2|y)
+nbc_x1_P_00 = length(Xtr1(Xtr1==0 & Ytr == 0))/Ytr0Length;
+nbc_x1_P_10 = length(Xtr1(Xtr1==1 & Ytr == 0))/Ytr0Length;
+nbc_x2_P_00 = length(Xtr1(Xtr2==0 & Ytr == 0))/Ytr0Length;
+nbc_x2_P_10 = length(Xtr1(Xtr2==1 & Ytr == 0))/Ytr0Length;
+nbc_x1_P_01 = length(Xtr1(Xtr1==0 & Ytr == 1))/Ytr1Length;
+nbc_x1_P_11 = length(Xtr1(Xtr1==1 & Ytr == 1))/Ytr1Length;
+nbc_x2_P_01 = length(Xtr1(Xtr2==0 & Ytr == 1))/Ytr1Length;
+nbc_x2_P_11 = length(Xtr1(Xtr2==1 & Ytr == 1))/Ytr1Length;
+
+%See hand working for classification and probabilities
+%Remember to put handwritten scans into report
+
+
+%% Section B: PCA 
 %
 % Eigenfaces - Part A to E
 %
@@ -83,7 +133,7 @@ title('Original Image');
 subplot(2,4,8); imagesc(reshape(img2,24,24)); axis square; colormap gray; 
 title('Original Image'); 
 
-%% Clustering 
+%% Section B: Clustering 
 %
 % Part A to E
 % 
@@ -147,6 +197,3 @@ title('K = 20');
 
 % EM Gaussian mixture model with k = 20 
 [z20_em,T,soft,ll] = emCluster(X_iris,20,'farthest',10); 
-
-
-
