b358ad78e7586c24786963e6b5dfc15922eb7470
tranquanghuy0801 <huytran.quang080199@gmail.com>
2019-05-23 18:57:17 +1000

    Finish the report

--- Matlab_Files/clustering_assignment.m
+++ Matlab_Files/clustering_assignment.m
@@ -13,7 +13,9 @@ warning('off','all');
 % Load the data and display a few faces 
 X = load('data/faces.txt'); 
 img = reshape(X(2,:),[24 24]); 
+figure(1); 
 imagesc(img); axis square; colormap gray;
+title('Display the image');
 
 %% Part A: Subtract the mean of the face images to make the data zero-mean 
 
@@ -32,26 +34,28 @@ for i = 1:length(K)
     mse_svd = mean(mean((X0 - X0_svd).^2)); % Compute MSE in SVD's approximation 
     errors(i) = mse_svd;     
 end 
-figure(1); 
+figure(2); 
 plot(K,errors); 
 title('Plot the MSE in SVD approximation against the K'); 
 xlabel('K'); ylabel('MSE'); 
 
 %% Part C: Display a first few principal directions of the data 
 
-lpha = 2 * median(abs(W(:,10))); % Scale factor 
+alpha = 2 * median(abs(W(:,10))); % Scale factor 
 direction1 = reshape(mean_X + alpha * V(:,10)',[24,24]); % 1st principal direction
 direction2 = reshape(mean_X - alpha * V(:,10)',[24,24]); % 2nd principal direction 
-figure(2); 
+figure(3); 
 imagesc(direction1); axis square; colormap gray; % Reshape and view as face image 
-figure(3);
+title('Direction+'); 
+figure(4);
 imagesc(direction2); axis square; colormap gray; % Reshape and view as face image 
+title('Direction-'); 
 
 
 %% Part D: Latent Space methods 
 
 idx = 15:25; % random indices of data 
-figure(4); title('Latent Space methods');  hold on; axis ij; colormap(gray); 
+figure(5); title('Latent Space methods');  hold on; axis ij; colormap(gray); 
 range = max(W(idx,1:2)) - min(W(idx,1:2)); % find range of coordinates to be plotted 
 scale = [200 200]./range;                  % want 24x24 to be visible 
 for i=1:length(idx)
@@ -67,7 +71,7 @@ index1 = indices(1); index2 = indices(2); % Two random indices
 % Choose random two images 
 img1 = X(index1,:); % Image 1 
 img2 = X(index2,:); % Image 2
-figure(5); 
+figure(6); 
 title('Construct image using K principal directions'); 
 for i = 1:length(K_recover)
     [U1 S1 V1] = svds(img1,K_recover(i)); % Take SVD of 1st image with different K 
@@ -95,7 +99,7 @@ title('Original Image');
 %% Part A: Load the usual Iris data with 2 features and plot 
 iris = load('data/iris.txt'); % Load Iris data 
 X_iris = iris(:,1:2);         % Use only two first features 
-figure(6); 
+figure(7); 
 plot(X_iris(:,1),X_iris(:,2),'ro'); 
 title('Plot of Iris data with 2 first features');
 
@@ -103,7 +107,7 @@ title('Plot of Iris data with 2 first features');
 
 % Choose the initialization with the best score 
 % Run k-means with k = 5 with farthest initialization 
-figure(7);
+figure(8);
 title('k-means on the data with k = 5'); 
 [z5 c5 sumd5] = kmeans(X_iris,5,'farthest',100); 
 plotClassify2D([],X_iris,z5); 
@@ -111,7 +115,7 @@ hold on
 plot(c5(:,1),c5(:,2),'kx'); 
 title('K-means K = 5'); 
 % Run k-means with k = 20 with k++ intialization 
-figure(8); 
+figure(9); 
 title('k-means on the data with k = 20'); 
 [z20 c20 sumd20] = kmeans(X_iris,20,'k++',100); 
 plotClassify2D([],X_iris,z20); 
@@ -124,7 +128,7 @@ title('K-means K = 20');
 % Using single linkage on Iiris data 
 [z5_min_agg join] = agglomCluster(X_iris,5,'min'); % k = 5 
 [z20_min_agg join] = agglomCluster(X_iris,20,'min'); % k = 20
-figure(9); 
+figure(10); 
 subplot(1,2,1); 
 plotClassify2D([],X_iris,z5_min_agg); 
 title('K = 5 Min'); 
@@ -134,7 +138,7 @@ title('K = 20 Min');
 % Using complete linkage on Iris data 
 [z5_max_agg join] = agglomCluster(X_iris,5,'max'); % k = 5
 [z20_max_agg join] = agglomCluster(X_iris,20,'max'); % k = 20
-figure(10); 
+figure(11); 
 subplot(1,2,1); 
 plotClassify2D([],X_iris,z5_max_agg); 
 title('K = 5 Max'); 
